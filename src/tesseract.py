e145d82743593b13a6153a2949a86f6c9a93b182
Peter Kral <peter.kral@pdfix.net>
2025-05-23 12:28:41 +0200

    Moving page rendering to separate script.

--- src/tesseract.py
+++ src/tesseract.py
@@ -5,73 +5,16 @@ import uuid
 import pytesseract
 from pdfixsdk.Pdfix import (
     GetPdfix,
-    PdfImageParams,
     Pdfix,
     PdfMatrix,
-    PdfPage,
-    PdfPageRenderParams,
-    kImageDIBFormatArgb,
-    kImageFormatJpg,
     kPdsPageText,
-    kPsTruncate,
-    kRotate0,
     kSaveFull,
 )
 from tqdm import tqdm
 
 import utils
 from exceptions import PdfixException
-
-
-def render_pages(page: PdfPage, pdfix: Pdfix, lang: str) -> bytes:
-    """
-    Render a PDF page into a temporary file, which is then used for OCR.
-
-    Args:
-        page (PdfPage): The PDF page to be processed for OCR.
-        pdfix (Pdfix): The Pdfix SDK object.
-        lang (str): The language identifier for OCR.
-
-    Returns:
-        Raw PDF bytes.
-    """
-    zoom = 2.0
-    page_view = page.AcquirePageView(zoom, kRotate0)
-    if page_view is None:
-        raise PdfixException("Unable to acquire page view")
-
-    width = page_view.GetDeviceWidth()
-    height = page_view.GetDeviceHeight()
-    # Create an image
-    image = pdfix.CreateImage(width, height, kImageDIBFormatArgb)
-    if image is None:
-        raise PdfixException("Unable to create image")
-
-    # Render page
-    render_params = PdfPageRenderParams()
-    render_params.image = image
-    render_params.matrix = page_view.GetDeviceMatrix()
-    if not page.DrawContent(render_params):
-        raise PdfixException("Unable to draw content")
-
-    # Create temp file for rendering
-    with tempfile.NamedTemporaryFile() as tmp:
-        # Save image to file
-        stm = pdfix.CreateFileStream(tmp.name + ".jpg", kPsTruncate)
-        if stm is None:
-            raise PdfixException("Unable to create file stream")
-
-        img_params = PdfImageParams()
-        img_params.format = kImageFormatJpg
-        img_params.quality = 100
-        if not image.SaveToStream(stm, img_params):
-            raise PdfixException("Unable to save image to stream")
-
-        return pytesseract.image_to_pdf_or_hocr(
-            tmp.name + ".jpg",
-            extension="pdf",
-            lang=lang,
-        )
+from page_renderer import render_pages
 
 
 def ocr(input_path: str, output_path: str, license_name: str, license_key: str, lang: str) -> None:
